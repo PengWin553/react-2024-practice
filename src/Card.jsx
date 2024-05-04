5e6d1e9836190ad859a4d279fe80f665cf3c7f5a
Winwin Kobayashi <victoriakobayashi553@gmail.com>
2024-05-04 11:23:11 +0800

    Created Cards

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+import profilePicture from './assets/images/cat cartoon.png'
+function Card(){
+    return(
+        <div className="card">
+            <img src={profilePicture} alt="profile picture" className="card-image" />
+            <h3 className="card-name">Pengwin</h3>
+            <p>I like coding and music.</p>
+        </div>
+    )
+}
+
+export default Card
